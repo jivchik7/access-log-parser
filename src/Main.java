71a7cc6963b52f19fc0a006990be350d4e1e7b1a
EVMinaeva <e.minaeva@vtb.ru>
2025-01-19 23:49:26 +0300

    Метод, который выводит результаты вычислений двух вводимых с клавиатуры чисел на экран.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " +
-                Math.random());
+        System.out.println("Введите первое число:");
+        int firstNumber = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число:");
+        int secondNumber = new Scanner(System.in).nextInt();
+
+        int sub = firstNumber + secondNumber;
+        int add = firstNumber - secondNumber;
+        int mul = firstNumber * secondNumber;
+        double div = ((double) (firstNumber))/secondNumber;
+
+        System.out.println("Сумма чисел: " + sub);
+        System.out.println("Разность чисел: " + add);
+        System.out.println("Произведение чисел: " + mul);
+        System.out.println("Частное чисел: " + div);
     }
 }
