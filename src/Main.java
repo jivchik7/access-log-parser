344f96beb3797b6c3239b9df0dd437a6287e9e48
EVMinaeva <e.minaeva@vtb.ru>
2025-02-10 01:01:21 +0300

    Создание циклической проверки существования файла по введенному пути с клавиатуры. Также проверяется указан путь к файлу или папке.

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите Enter: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
-    }
-}
